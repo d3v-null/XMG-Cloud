85b5556903c8327f7df81323e06170be03d3033c
derwentx <derwentx@gmail.com>
2018-11-06 14:10:30 +1100

    no need for testbed in cloudSQL

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,14 +9,6 @@ from . import config, layers
 
 from oauth2client.client import HttpAccessTokenRefreshError
 
-if True:
-    import dev_appserver
-    dev_appserver.fix_sys_path()
-
-    from google.appengine.datastore import datastore_stub_util
-    from google.appengine.ext import ndb
-    from google.appengine.ext import testbed
-
 
 @pytest.yield_fixture
 def app(request):
@@ -53,23 +45,6 @@ def model(monkeypatch, app):
     Note: Originally this fixture provided access to multiple models
     """
 
-    # First, create an instance of the Testbed class.
-    ds_testbed = testbed.Testbed()
-    # Then activate the testbed, which prepares the service stubs for use.
-    ds_testbed.activate()
-    # Create a consistency policy that will simulate the High Replication
-    # consistency model.
-    policy = datastore_stub_util.PseudoRandomHRConsistencyPolicy(
-        probability=1)
-    # Initialize the datastore stub with this policy.
-    ds_testbed.init_datastore_v3_stub(consistency_policy=policy)
-    # Initialize memcache stub too, since ndb also uses memcache
-    ds_testbed.init_memcache_stub()
-    # Clear in-context cache before each test.
-    ndb.get_context().clear_cache()
-    # Initialize app identity stub
-    ds_testbed.init_app_identity_stub()
-
     # Ensure no layers exist before running. This typically helps if tests
     # somehow left the database in a bad state.
     delete_all_layers(layers.model)
@@ -78,8 +53,6 @@ def model(monkeypatch, app):
 
     # Delete all layers that we created during tests.
     delete_all_layers(layers.model)
-
-    ds_testbed.deactivate()
 
 
 
